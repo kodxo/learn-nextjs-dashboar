e993bc0bf01f159f9a428e954368bd6db22bfe44
Jessy Pango <kvnjessy@gmail.com>
2026-05-08 14:06:12 +0100

    Link et logo

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,12 +1,19 @@
+import Link from "next/link";
+import AcmeLogo from "../acme-logo";
 import NavLinks from "./nav-links";
 
 export default function Sidenav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      {/*  Liens */}
+      <Link
+        href="/"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+      >
+        <div className="w-32 text-white md:w-40">
+          <AcmeLogo />
+        </div>
+      </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        {/* Liens de Navigation */}
-
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block">
           <NavLinks />
         </div>
